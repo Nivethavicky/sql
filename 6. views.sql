d62d31e1fe723df68dbf8cd9bc2f082d8081f070
Nivethavicky <167093019+Nivethavicky@users.noreply.github.com>
2024-05-29 07:51:39 +0530

    Add files via upload

---
+++ 6. views.sql
@@ -0,0 +1,34 @@
+-- complex query handle
+use sql_class;
+
+select employee.emp_id , employee.ename, branch.br_name 
+from employee inner join branch on employee.branch_id = branch.branch_id 
+order by employee.emp_id;
+
+create view emp_branch
+as
+select employee.emp_id , employee.ename, branch.br_name 
+from employee inner join branch on employee.branch_id = branch.branch_id 
+order by employee.emp_id;
+
+
+select * from emp_branch;
+
+select * from emp_branch where ename="Abdul";
+
+select * from employee where br_name="sam";
+
+
+create or replace view emp_branch
+as
+select employee.emp_id , employee.ename, branch.br_name 
+from employee inner join branch on employee.branch_id = branch.branch_id ;
+
+drop view emp_branch;
+
+
+SHOW FULL TABLES
+WHERE Table_Type LIKE 'VIEW';
+
+
+SHOW FULL TABLES;
